8efb8786f71ea26d63b645f0c64574e2d440e17e
Melody <leclecticat@gmail.com>
2020-06-11 14:38:44 -0700

    initial drag and drop setup

--- src/App.js
+++ src/App.js
@@ -4,11 +4,22 @@ import './App.css';
 import React, { useCallback } from "react";
 import cuid from "cuid";
 import Dropzone from "./Dropzone";
+import { DndProvider } from "react-dnd";
+import HTML5Backend from "react-dnd-html5-backend";
+import update from "immutability-helper";
 
 function App() {
   const [images, setImages] = useState([]);
 
-
+  const moveImage = (dragIndex, hoverIndex) => {
+   
+    const draggedImage = images[dragIndex];
+    setImages(
+      update(images, {
+        $splice: [[dragIndex, 1], [hoverIndex, 0, draggedImage]]
+      })
+    );
+};
 
   const onDrop = useCallback(acceptedFiles => {
     // Loop through accepted files
@@ -29,7 +40,9 @@ function App() {
     <main className="App">
       <h1 className="text-center">Drag and Drop Example</h1>
       <Dropzone onDrop={onDrop} accept={"image/*"} />
-      <ImageList images={images} />
+      <DndProvider backend={HTML5Backend}>
+        <ImageList images={images} />
+      </DndProvider>
     </main>
   );
 }
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useDrag, useDrop } from "react-dnd";
 
-const Image = ({ image }) => {
+const type = "Image";
+
+const Image = ({ image, index }) => {
+    const ref = useRef(null);
+
+    const [, drop] = useDrop({
+        accept: type,
+        hover(item) {
+            if (!ref.current) {
+                return;
+              }
+              const dragIndex = item.index;
+              const hoverIndex = index;
+              if (dragIndex === hoverIndex) { 
+                return;
+              }
+              moveImage(dragIndex, hoverIndex);
+              item.index = hoverIndex;
+        }
+    });
+
+    const [{ isDragging }, drag] = useDrag({
+        item: { type, id: image.id, index },
+        collect: monitor => ({
+        isDragging: monitor.isDragging()
+        })
+    });
+  
+    drag(drop(ref));
+
     return (
-      <div className="file-item">
+      <div ref={ref} className="file-item"
+      style={{ opacity: isDragging ? 0 : 1 }} >
+
         <img alt={`img - ${image.id}`} src={image.src} className="file-img" />
       </div>
     );
